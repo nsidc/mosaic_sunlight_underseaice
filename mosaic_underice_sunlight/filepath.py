f972629bd0b444075ae53c9a769f4fdfe00362ce
Andy Barrett <apbarret@nsidc.org>
2024-04-04 11:53:12 -0600

    update filepaths and add metdata filepaths

--- mosaic_underice_sunlight/filepath.py
+++ mosaic_underice_sunlight/filepath.py
@@ -3,4 +3,21 @@ from pathlib import Path
 
 DATAPATH = Path.home() / 'Data' / 'Sunlight_under_seaice' / 'MOSAiC_Observations'
 RAW_DATAPATH = DATAPATH / 'raw'
+REFORMAT_DATAPATH = DATAPATH / 'reformat' / 'MOSAiC_magnaprobe'
 PROCESSED_DATAPATH = DATAPATH / 'processed'
+CLEAN_DATAPATH = DATAPATH / 'clean'
+
+MET_DATAPATH = DATAPATH / 'Meteorology'
+MET_TOWER_DATAPATH = MET_DATAPATH / 'tower' / '3_level_archive' / 'level3.4'
+MET_ASFS30_DATAPATH = MET_DATAPATH / 'asfs30' / '3_level_archive_asfs30' / 'level3.4'
+MET_ASFS40_DATAPATH = MET_DATAPATH / 'asfs40' / '3_level_archive_asfs40' / 'level3.4'
+MET_ASFS50_DATAPATH = MET_DATAPATH / 'asfs50' / '3_level_archive_asfs50' / 'level3.4'
+
+MET_DATAPATHS = {
+    'tower': MET_TOWER_DATAPATH,
+    'asfs30': MET_ASFS30_DATAPATH,
+    'asfs40': MET_ASFS40_DATAPATH,
+    'asfs50': MET_ASFS50_DATAPATH,
+    }
+
+FILLED_MET_DATAPATH = DATAPATH / 'Meteorology' / 'gap_filled'
