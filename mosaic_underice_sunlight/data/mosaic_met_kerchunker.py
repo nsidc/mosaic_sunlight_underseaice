e1f0c6b0c425a157d633dbdc97b7b6e0564cd953
Andy Barrett <apbarret@nsidc.org>
2024-04-24 09:53:15 -0600

    add doc strings and cli

--- mosaic_underice_sunlight/data/mosaic_met_kerchunker.py
+++ mosaic_underice_sunlight/data/mosaic_met_kerchunker.py
@@ -7,7 +7,9 @@ import os
 import ujson
 import re
 import warnings
-from typing import List
+from typing import List, Union
+import tempfile
+from urllib.parse import urlparse
 
 import fsspec  # For interfacing with file systems
 from kerchunk.hdf import SingleHdf5ToZarr  # The magic sauce
@@ -18,13 +20,14 @@ from pqdm.threads import pqdm  # To parallelize
 from mosaic_underice_sunlight.filepath import MET_DATAPATHS
 
 
-def get_filelist(fs: fsspec.filesystem, url: str, glob: str="*") -> List:
+def get_filelist(url: str, glob: str="*") -> List:
     """Returns iterable of local or remote filepaths to create mappers
 
     fs : fileystem
     url : filepath or url
     glob : glob pattern to search for.  Default is all
     """
+    fs = fsspec.filesystem(get_protocol(url)) 
     return fs.glob(f"{url}/{glob}")
 
 
@@ -61,14 +64,14 @@ def gen_json(fname: str, outdir: Union[str, Path]='.') -> Union[str, Path]:
               default_fill_cache=False, 
               default_cache_type='first')
 
-    with fs.open(fname, **so) as infile:
+    with fsspec.open(fname, **so) as infile:
         h5chunks = SingleHdf5ToZarr(infile, fname, inline_threshold=300)
-        outf = make_json_name(fname, dout='temp')
+        outf = make_json_name(fname, dout=outdir)
         outf.parent.mkdir(parents=True, exist_ok=True)
-        with fs.open(outf, 'wb') as f:
+        with fsspec.open(outf, 'wb') as f:
             f.write(ujson.dumps(h5chunks.translate()).encode());
 
-    return outf
+    return str(outf)
 
 
 def generate_single_jsons(filelist: List[str], outdir: str='.', n_jobs: int=4) -> List[str]:
@@ -84,7 +87,9 @@ def generate_single_jsons(filelist: List[str], outdir: str='.', n_jobs: int=4) -
     -------
     List of filepaths to JSON reference files
     """
-    result = pqdm(flist, gen_json, n_jobs=4)
+    args = [[f, outdir] for f in filelist]
+    result = pqdm(args, gen_json, n_jobs=4, argument_type='args')
+    return result
 
 
 def generate_multizarr_json(jsonlist: List[Union[str, Path]],
@@ -101,19 +106,26 @@ def generate_multizarr_json(jsonlist: List[Union[str, Path]],
     None
     """
     mzz = MultiZarrToZarr(
-        json_list,
+        jsonlist,
         concat_dims=['time'],
         coo_map = {"time": "cf:time"},
         )
 
     d = mzz.translate()
-    with fs.open(multijson, 'wb') as f:
+    with fsspec.open(multijson, 'wb') as f:
         f.write(ujson.dumps(d).encode())
 
     return None
 
 
-def mosaic_met_kerchunker(site: str, jsonname: Union[str, Path]) -> None:
+def get_protocol(url):
+    """Returns the protocol based on urllib scheme"""
+    return urlparse(str(url)).scheme
+
+
+def mosaic_met_kerchunker(site: str,
+                          jsonname: Union[str, Path],
+                          test: bool=False) -> None:
     """Generates multizarr json files for data from a MOSAiC met site
 
     Parameters
@@ -126,18 +138,55 @@ def mosaic_met_kerchunker(site: str, jsonname: Union[str, Path]) -> None:
     None
     """
 
+    tempdirname = Path('temp')
+    
     try:
         url = MET_DATAPATHS[site]
     except KeyError as err:
         raise KeyError(f"Unknown met site identifier.  "
                        f"Expected one of {', '.join(MET_DATAPATHS.keys())}")
-        
-    src_fs = fsspec.filesystem()
-    filelist = get_filelist(src_fs, url, "mosmet*.nc")
 
-    jsonlist = generate_single_jsons(filelist, outdir, n_jobs=4)
+#    src_fs = fsspec.filesystem(get_protocol(url))
+    filelist = get_filelist(url, "mosmet*.nc")
 
+    if test:
+        filelist = filelist[:10]
+    
+    jsonlist = generate_single_jsons(filelist, tempdirname, n_jobs=4)
     generate_multizarr_json(jsonlist, jsonname)
+
+    cleanup(tempdirname)
+
+
+def cleanup(tempdirname):
+    for root, dirs, files in tempdirname.walk(top_down=False):
+        for name in files:
+            (root / name).unlink()
+        for name in dirs:
+            print(root / name)
+    if ('.' / tempdirname).exists() & (str(tempdirname) != '.'):
+        tempdirname.rmdir()
+    else:
+        warnings.warn(f"{tempdirname} is current working directory: did not remove it")
+
+
+def load_metdata_zarr(jsonmzz: Union[str, Path]) -> xr.Dataset:
+    """Loads a multizarr file"""
+    backend_args = {
+        "consolidated": False,
+        "storage_options": {
+            "fo": str(jsonmzz),
+            }
+        }
+
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        ds = xr.open_dataset(
+            "reference://",
+            engine="zarr",
+            backend_kwargs=backend_args
+            )
+    return ds
 
 
 if __name__ == "__main__":
@@ -149,7 +198,9 @@ if __name__ == "__main__":
                         help="identifier for MOSAiC met site")
     parser.add_argument("jsonname", type=str,
                         help="filepath to save multizarr json reference file")
+    parser.add_argument("--test", action='store_true',
+                        help="run test.  Only processes first 10 files")
 
     args = parser.parse_args()
 
-    mosaic_met_kerchunker(args.site, args.jsonname)
+    mosaic_met_kerchunker(args.site, args.jsonname, test=args.test)
