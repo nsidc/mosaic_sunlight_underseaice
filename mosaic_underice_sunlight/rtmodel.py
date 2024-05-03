5a08485186f54b7bd31e10c893c3f8c590fbd454
Andy Barrett <apbarret@nsidc.org>
2024-05-03 16:33:13 -0600

    add function for irradiance to quanta calculations

--- mosaic_underice_sunlight/rtmodel.py
+++ mosaic_underice_sunlight/rtmodel.py
@@ -9,6 +9,145 @@ underice_flux2par = 3.5   # from Eq 10
 openwater_flux2par = 2.3  # from Eq 9 Stroeve et al
 
 
+def sw2par_underice(irradiance: float) -> float:
+    """Returns estimated photosynthetic active 
+    radiation portion of SW flux density beneath a sea 
+    ice column in W/m^2
+
+    Parameters
+    ----------
+    irradiance : broadband SW flux density in W/m2
+
+    Returns
+    -------
+    par in W/m2
+
+    Broadband SW flux density is integral of spectral irradiance
+    between 200 nm and 4000 nm.  PAR radiative flux density is
+    the integral of spectral irradiance over 400 nm to 700 nm.  
+    The bulk conversion factor for underice light is estimated
+    from measurements made during the Greenedge campaign.
+
+    F_PAR / F_SW = 0.79 +/- 0.003
+
+    E.g. F_PAR is 79% of broadband SW radiative flux density
+    
+    See Stroeve, Julienne, Martin Vancoppenolle, Gaelle Veyssiere, 
+    Marion Lebrun, Giulia Castellani, Marcel Babin, Michael Karcher, 
+    Jack Landy, Glen E. Liston, and Jeremy Wilkinson. 
+    “A Multi-Sensor and Modeling Approach for Mapping Light Under 
+    Sea Ice During the Ice-Growth Season.” 
+    Frontiers in Marine Science 7 (2021). 
+    https://doi.org/10.3389/fmars.2020.592337.
+    """
+    SW2PAR = 0.79
+    return irradiance * SW2PAR
+
+
+def sw2par_openwater(irradiance: float) -> float:
+    """Returns the estimated phtosynthetically active portion
+    of broadband SW irradiance for open sea water in W/m2
+
+    Parameters
+    ----------
+    irradiance : broadband SW flux density in W/m2
+
+    Returns
+    -------
+    par in W/m2
+
+    Broadband SW flux density is integral of spectral irradiance
+    between 200 nm and 4000 nm.  PAR radiative flux density is
+    the integral of spectral irradiance over 400 nm to 700 nm.  
+    The conversion factor is taken from Frouin and Pinker (1995).  
+    They give the range of conversion factors as 0.45 to 0.5.
+
+    F_PAR / F_SW = 0.5
+
+    E.g. PAR is 50 % of SW flux.
+
+    Frouin, Robert, and Rachel T. Pinker. “Estimating Photosynthetically 
+    Active Radiation (PAR) at the Earth’s Surface from Satellite 
+    Observations.” Remote Sensing of Environment, Remote Sensing of 
+    Land Surface for Studies of Global Chage, 51, no. 1 
+    (January 1, 1995): 98–107. https://doi.org/10.1016/0034-4257(94)00068-X.
+    """
+    SW2PAR = 0.5
+    return irradiance * SW2PAR
+
+
+def rad2quanta_underice(irradiance: float) -> float:
+    """Returns an irradiance in W/m2 as quanta/m2/s for
+    underice light
+
+    Parameter
+    ---------
+    irradiance : irradiance in W/m2
+
+    Returns
+    -------
+    quantum measure of irrandiance as micromoles photons/m2/s
+
+    The quantum measure of radiative flux density for PAR is
+    given as:
+    
+    $$
+    Q = \frac{1}{hc}\int_{400 nm}^{700 nm} I(\lambda) \lambda d\lambda
+    $$
+
+    where $h$ is the Planck constant and $c$ is the speed of light.
+
+    The conversion factor for broadband SW used here is from the Greenedge campaign
+
+    Q_PAR / F_PAR = 4.44 +/- 0.005 micromoles photons per J
+    """
+    RAD2QUANTA = 4.44
+    return irradiance * RAD2QUANTA
+
+
+def rad2quanta_openwater(irradiance: float) -> float:
+    """Returns an irradiance in W/m2 as quanta/m2/s for
+    underice light
+
+    Parameter
+    ---------
+    irradiance : irradiance in W/m2
+
+    Returns
+    -------
+    quantum measure of irrandiance as micromoles photons/m2/s
+
+    The quantum measure of radiative flux density for PAR is
+    given as:
+    
+    $$
+    Q = \frac{1}{N_A hc}\int_{400 nm}^{700 nm} I(\lambda) \lambda d\lambda
+    $$
+
+    where $h$ is the Planck constant and $c$ is the speed of light.
+
+    The conversion factor for broadband SW used here is from Morel and Smith
+
+    Q_PAR / F_PAR = 4.44 +/- 0.005 micromoles photons per J
+
+    This comes from the Green water estimate in Table 2 of Morel and Smith (1974),
+    which gives the conversion factor in units quanta / J.  Dividing by the 
+    Avogadro constant (6.022e23) gives the CF in micromoles per J
+    """
+    RAD2QUANTA = 4.44
+    return irradiance * RAD2QUANTA
+
+
+def get_qpar_openwater(irradiance: float) -> float:
+    """Returns PAR in micromoles per meter squared per second"""
+    return rad2quanta_openwater( sw2par_openwater( irradiance) )
+
+
+def get_qpar_underice(irradiance: float) -> float:
+    """Returns PAR in micromoles per meter squared per second"""
+    return rad2quanta_underice( sw2par_underice( irradiance) )
+
+
 def flux_to_par(df):
     """Converts radiative flux to PAR based on whether ice is present or not
 
