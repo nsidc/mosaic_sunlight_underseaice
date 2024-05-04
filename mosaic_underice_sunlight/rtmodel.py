c9b2f441ebf2ac7de2f3fec682109a9a3b27a064
Andy Barrett <apbarret@nsidc.org>
2024-05-03 18:09:23 -0600

    refactor seaicert_mp to use seaicert_point

--- mosaic_underice_sunlight/rtmodel.py
+++ mosaic_underice_sunlight/rtmodel.py
@@ -1,5 +1,5 @@
 """Contains functions to run seaice_rt model"""
-from typing import Union
+from typing import Union, Tuple, List
 import datetime as dt
 
 import pandas as pd
@@ -277,71 +277,20 @@ def seaicert_mp(df: pd.DataFrame) -> pd.DataFrame:
     pandas.DataFrame containing shortwave absorbed flux absorbed by ocean, 
     total radiation absorbed by ocean, albedo and surface shortwave flux
     """
-    
-    model = SeaIceRT()
 
-    model.snow_grain_radius = 180.
+    # Fields in dataframe to use as forcing for seaicert
+    fields = [
+        "lat",
+        "snow_depth_m",
+        "melt_pond_depth_m",
+        "ice_thickness_m",
+        "surface_temperature_K",
+        "air_temperature_K"
+    ]
 
-    day_of_year = df.index.day_of_year
-    lat = df.lat.values
-    snow_depth_m = df.snow_depth_m.values
-    melt_pond_depth_m = df.melt_pond_depth_m.values
-    ice_thickness_mean_m = df.ice_thickness_mean_m.values
-    
-    thiszip = zip(
-        day_of_year,
-        lat,
-        snow_depth_m,
-        melt_pond_depth_m,
-        ice_thickness_mean_m,
-    )
-    
-    distance = []
-    sw_absorbed_by_ocean = []
-    lw_absorbed_by_ocean = []
-    surface_albedo = []
-    surface_downwelling_radiative_flux = []
-    #for idx, vals in df.iterrows():
-    for iday_of_year, ilat, isnow_depth_m, imelt_pond_depth_m, iice_thickness_mean_m in thiszip:
-        if (isnow_depth_m > 0.) & (imelt_pond_depth_m > 0.):
-            raise ValueError(f"Snow depth and pond depth cannot both be greater than zero: {iday_of_year}")
-        if (isnow_depth_m < 0.):
-            raise ValueError(f"Snow depth must be positive")
-        if (imelt_pond_depth_m < 0.):
-            raise ValueError(f"Melt pond depth must be positive")
-        if (iice_thickness_mean_m < 0.):
-            raise ValueError(f"Ice thickness must be positive")
-        model.day_of_year = iday_of_year + 0.5  # adjust for longitude?
-        model.latitude = ilat
-        model.snow_depth = isnow_depth_m
-        model.pond_depth = imelt_pond_depth_m
-        model.sea_ice_thickness = iice_thickness_mean_m
-    
-        model.run()
-        output = model.get_results()
-        #distance.append(vals.transect_distance_m)
-        sw_absorbed_by_ocean.append(output["downwelling_shortwave_flux_absorbed_by_ocean"])
-        lw_absorbed_by_ocean.append(output["downwelling_longwave_flux_absorbed_by_ocean"])
-        surface_albedo.append(output["surface_albedo"])
-        surface_downwelling_radiative_flux.append(output["surface_downwelling_radiative_flux"])
-        
-    result = pd.DataFrame(
-        {
-            'datetime': df.index.values,
-            'latitude': df.lat.values,
-            'snow_depth_m': df.snow_depth_m.values,
-            'melt_pond_depth_m': df.melt_pond_depth_m.values,
-            'ice_thickness_mean_m': df.ice_thickness_mean_m.values,
-            'sw_absorbed_by_ocean': sw_absorbed_by_ocean,
-            'downwelling_radiative_flux_absorbed_by_ocean': np.array(sw_absorbed_by_ocean) + np.array(lw_absorbed_by_ocean),
-            'surface_albedo': surface_albedo,
-            'surface_downwelling_radiative_flux': surface_downwelling_radiative_flux,
-            'transect_distance_m': df.transect_distance_m.values,
-        },
-        index = df.transect_distance_m,
-    )
+    result = [seaicert_point(time, *values) for time, values in df[fields].iterrows()]
 
-    # Convert radiative flux to ocean to PAR
-    flux_to_par(result)
-    
-    return result
+    # Parse results into pandas.DataFrame
+    rt_df = df.join(results_to_dataframe(result))
+
+    return rt_df
